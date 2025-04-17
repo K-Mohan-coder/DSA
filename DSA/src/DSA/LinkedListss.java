61a9de8e7ab99b60978e4145aa367eec76e1750e
Mohan <kmohan17official@gmail.com>
2025-04-17 19:30:55 +0530

    Day-2

--- DSA/src/DSA/LinkedListss.java
+++ DSA/src/DSA/LinkedListss.java
@@ -70,12 +70,49 @@ class LinkedList {
         }
         System.out.println();
     }
+    
+ // Delete Operations
+    public void deleteFromBeginning() {
+            if (head == null)
+                return;
+            head = head.next;
+        }
+    
+ // Deletion from the end of the linked list
+    public void deleteFromEnd() {
+        if (head == null || head. next == null) {
+            head = null;
+            return;
+        }
+        Node current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+    }
+    
+ // Deletion from the middle of the linked list
+    public void deleteFromMiddle (int position) {
+        if (position == 0 || head == null) {
+            if (head != null)
+                head = head.next;
+            return;
+        }
+        Node current = head;
+        for (int i = 0; i < position && current.next != null; i++) {
+            current = current.next;
+        }
+        if (current == null || current.next == null)
+            throw new IllegalArgumentException("Position is out of bounds");
+        current.next = current.next.next;
+    }
 }
 
 public class LinkedListss {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
-
+        
+        System.out.println("Adding Data in Linked List");
         // Adding nodes
         list.insertAtBeginning(1);
         list.insertAtEnd(4);
@@ -86,5 +123,13 @@ public class LinkedListss {
 
         // Printing list
         list.printList();  // Output: 0 1 2 3 4 5
+        
+        System.out.println("Deleting Data in Linked List");
+        
+        list.deleteFromBeginning();
+        list.deleteFromEnd();
+        list.deleteFromMiddle(2);
+        list.printList();
+
     }
 }
